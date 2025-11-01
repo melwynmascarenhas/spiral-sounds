96562007e56880588f40874c6c43737aed685246
Melwyn <121210177+melwynmascarenhas@users.noreply.github.com>
2025-11-01 18:20:03 +0530

    Update productUI.js

--- public/js/productUI.js
+++ public/js/productUI.js
@@ -1,11 +1,13 @@
-import { addBtnListeners } from './cartService.js'
+import { addBtnListeners } from "./cartService.js";
+import { getProducts } from "./productService.js";
 
 // ===== Rendering products =====
 
 export function renderProducts(products) {
-  const albumsContainer = document.getElementById('products-container')
-  const cards = products.map((album) => {
-    return `
+  const albumsContainer = document.getElementById("products-container");
+  const cards = products
+    .map((album) => {
+      return `
       <div class="product-card">
         <img src="./images/${album.image}" alt="${album.title}">
         <h2>${album.title}</h2>
@@ -14,19 +16,20 @@ export function renderProducts(products) {
         <button class="main-btn add-btn" data-id="${album.id}">Add to Cart</button>
         <p class="genre-label">${album.genre}</p>
       </div>
-    `
-  }).join('')
+    `;
+    })
+    .join("");
 
-  albumsContainer.innerHTML = cards
-  addBtnListeners()
+  albumsContainer.innerHTML = cards;
+  addBtnListeners();
 }
 
 // ===== Handling filtering =====
 
 export async function applySearchFilter() {
-  const search = document.getElementById('search-input').value.trim()
-  const filters = {}
-  if (search) filters.search = search
-  const products = await getProducts(filters)
-  renderProducts(products)
+  const search = document.getElementById("search-input").value.trim();
+  const filters = {};
+  if (search) filters.search = search;
+  const products = await getProducts(filters);
+  renderProducts(products);
 }
